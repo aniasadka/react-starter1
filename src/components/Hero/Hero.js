a7ca732992865269a4bc29d995b4e51fe19c3767
Anna Sadka <anna.sadka87@gmail.com>
2020-10-18 22:10:51 +0200

    add Hero.js

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Hero.scss';
+
+
+const Hero = () => (
+  <header className={styles.component}>
+    <h2 className={styles.title}>Things to do</h2>
+    <img className={styles.image} src= 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png' />
+  </header>
+);
+
+
+export default Hero;
+
+/*dla komponentu funkcyjnego:
+import React from 'react';
+import styles from './MyComponent.scss';
+const MyComponent = () => (
+  <div>
+    <h3>Hello world!</h3>
+  </div>
+);
+export default MyComponent;
+*/
