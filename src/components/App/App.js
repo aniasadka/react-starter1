238eb8a7b4bc4032d9c5f7e1b0fe6f67f1617188
Anna Sadka <anna.sadka87@gmail.com>
2020-10-18 21:43:56 +0200

    change h2 in list.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './App.scss';
 
+
 class App extends React.Component {
   render() {
     return (
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './List.scss';
 
+
 class List extends React.Component {
   render() {
     return (
       <section className={styles.component}>
       
-        <h2 className={styles.subtitle}>Hello world!</h2>
+        <h2 className={styles.subtitle}>Things to do</h2>
       </section>
     )
   }
