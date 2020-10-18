70ce8c58403fc420e7d4e32c46a846c4ba532b9d
Anna Sadka <anna.sadka87@gmail.com>
2020-10-18 21:21:50 +0200

    change main to section and remove h1

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -4,10 +4,10 @@ import styles from './List.scss';
 class List extends React.Component {
   render() {
     return (
-      <main className={styles.component}>
-        <h1 className={styles.title}>My first React app</h1>
+      <section className={styles.component}>
+      
         <h2 className={styles.subtitle}>Hello world!</h2>
-      </main>
+      </section>
     )
   }
 }
