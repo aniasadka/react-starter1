65b697e1e38b9d17c1ee43de25621bfef66c43cf
Anna Sadka <anna.sadka87@gmail.com>
2020-10-19 14:31:18 +0200

    Task 15.4 - part 2

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,9 +9,12 @@ class App extends React.Component {
       <main className={styles.component}>
         <h1 className={styles.title}>What should I do today?</h1>
         <h2 className={styles.subtitle}>Here is a list:</h2>
-        <List title={['Things to do ', <sup key='1'>soon!</sup>]} >
+        <List title={['Things to do ', <sup key='1'>soon!</sup>]} image='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'>
         <p>I'm planning on doing all these things sooner, rather than later!</p>
+        
         </List>
+
+       
       </main>
     )
   }
@@ -19,7 +22,3 @@ class App extends React.Component {
 
 export default App;
 
-
-//<List title={['Things To Do', <sup key="1"></sup>]} image='http://uploads.kodilla.com/bootcamp/fer/11.react/space.png'>
-         
-//</List>
--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import styles from './Column.scss';
+import PropTypes from 'prop-types';
 
 class Column extends React.Component {
     render() {
         return (
         <section className={styles.component}>
-            <h3 className={styles.title}></h3>
+            <h3 className={styles.title}>{this.props.titleText}</h3>
         </section>
         )
     }
 }
+Column.propTypes = {
+    titleText: PropTypes.node
+};
 
 export default Column; 
