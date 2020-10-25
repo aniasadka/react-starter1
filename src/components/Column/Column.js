a61d471ee044c775ebc505421d10963a049d22f5
Anna Sadka <anna.sadka87@gmail.com>
2020-10-25 19:40:40 +0100

    Change titleText for title - task 15.5

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -6,13 +6,13 @@ class Column extends React.Component {
     render() {
         return (
         <section className={styles.component}>
-            <h3 className={styles.title}>{this.props.titleText}</h3>
+            <h3 className={styles.title}>{this.props.title}</h3>
         </section>
         )
     }
 }
     Column.propTypes = {
-        titleText: PropTypes.node.isRequired,
-    };
+        title: PropTypes.node,
+};
 
 export default Column; 
