db1fe2aa28889ec82be34d79d4c9414d52d301e7
Anna Sadka <anna.sadka87@gmail.com>
2021-01-10 23:30:52 +0100

    error

---
+++ src/components/Faq/Faq.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from '../Container/Container';
+import {faq} from '../..data/dataStore';
+//import Hero from '..Hero/Hero';
+import styles from './Faq.scss';
+
+const Faq = () => (
+    <Container>
+        <Hero titleText={faq.title} image={faq.image} />
+        <p>{faq.description}</p>
+    </Container>
+);
+
+export default Faq;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,26 +2,35 @@ import React from 'react';
 import {NavLink, Link} from 'react-router-dom';
 import styles from './Header.scss';
 import Container from '../Container/Container';
+import { settings } from '../../data/dataStore';
 import Icon from '../Icon/Icon';
 
 class Header extends React.Component {
   render(){
     return (
       <header className={styles.component}>
-        <Container>
-          <div className={styles.wrapper}>
-            <Link to='/' className={styles.logo}>
-              <Icon name='cat' />
-            </Link>
-            <nav>
-              <NavLink exact to='/'>Home</NavLink>
-              <NavLink exact to='/info'>Info</NavLink>
-            </nav>
-          </div>
-        </Container>
-      </header>
-    );
-  }
+      <Container>
+        <Search />
+        <div className={styles.wrapper}>
+          <Link className={styles.logo} to='/'>
+            <Icon name={settings.header.icon} />
+          </Link>
+          <nav>
+            <NavLink exact to='/' activeClassName='active'>
+              Home
+            </NavLink>
+            <NavLink exact to='/info' activeClassName='active'>
+              Info
+            </NavLink>
+            <NavLink exact to='/faq' activeClassName='active'>
+              FAQ
+            </NavLink>
+          </nav>
+        </div>
+      </Container>
+    </header>
+  );
+}
 }
 
 export default Header;
