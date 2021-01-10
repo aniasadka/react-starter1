d798015a539c71f7cc253ef0c138f890843d349c
Anna Sadka <anna.sadka87@gmail.com>
2021-01-10 22:53:49 +0100

    change dataStore

--- src/data/dataStore.js
+++ src/data/dataStore.js
@@ -10,8 +10,25 @@ export const settings = {
     defaultText: 'Search...',
     icon: 'search',
   },
+
+  header: {
+    icon: 'cat',
+  },
+  
   defaultListDescription: '<p>I can do all the things!!!</p>',
   defaultColumnIcon: 'list-alt',
+};
+
+export const info = {
+  title: 'Info',
+  description: 'INFO: Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png', 
+};
+
+export const faq = {
+  title: 'FAQ',
+  description: 'FAQ: Lorem ipsum dolor sit amet, consectetur adipiscing elit laborum!!!',
+  image: 'http://uploads.kodilla.com/bootcamp/fer/11.react/space.png',
 };
 
 export const pageContents = {
