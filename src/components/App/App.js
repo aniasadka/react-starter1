6c7d231c3c50b291a80e3c99a268c1c76aa688d8
Anna Sadka <anna.sadka87@gmail.com>
2021-01-11 21:19:09 +0100

    17.3 -animowane zmiany widoku

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,15 +4,20 @@ import Info from '../Info/Info';
 import Faq from '../Faq/Faq';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import MainLayout from '../MainLayout/MainLayout';
+import {AnimatedSwitch} from 'react-router-transition';
 
 const App = () => (
   <BrowserRouter>
     <MainLayout>
-      <Switch>
+    <AnimatedSwitch
+      atEnter={{ opacity: 0 }}
+      atLeave={{ opacity: 0 }}
+      atActive={{ opacity: 1 }}
+      className="switch-wrapper">
         <Route exact path='/' component={Home} />
         <Route exact path='/info' component={Info} />
         <Route exact path='/faq' component={Faq} />
-      </Switch>
+        </AnimatedSwitch>
     </MainLayout>
   </BrowserRouter>
 );
